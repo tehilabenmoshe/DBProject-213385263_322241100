c71de1ceb53127eb7b7452d3e005d50914be87dd
tehilabenmoshe <117288122+tehilabenmoshe@users.noreply.github.com>
2024-06-14 13:20:27 +0300

    Add files via upload

---
+++ part 1/dropTables.sql
@@ -0,0 +1,7 @@
+DROP TABLE Course_tool;
+DROP TABLE Registration;
+DROP TABLE Tool;
+DROP TABLE Course;
+DROP TABLE Participant;
+DROP TABLE Instructor;
+DROP TABLE TCategory;
---
+++ part 1/selectAll.sql
@@ -0,0 +1,8 @@
+select * from Course
+select * from Instructor
+select * from Participant
+select * from Course_tool
+select * from Registration
+select * from Tool
+select * from TCategory
+
